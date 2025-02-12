947ff2ec061fdfdf5e811a7509eb6305a981bb6d
Gabriela Schaper <gabrielasch2409@gmail.com>
2025-02-12 13:33:55 -0300

    upload

---
+++ Arquivos/app.py
@@ -0,0 +1,48 @@
+
+
+
+#nome = input('Digite o seu nome: ')
+
+#print(f'Olá {nome}, seja muito bem vindo')
+
+
+#n1 = input('Digite a sua n1:')
+#n2 = input('Digite a sua n2:')
+
+#print(int(n1)+int(n2))
+
+
+#n1 = int(input('Digite uma nota: '))
+#n2 = int(input('Digite uma nota: '))
+
+#media = (n1 + n2) / 2
+
+
+#print(media)
+
+#trfs = 5 < 6
+
+#print(trfs)
+
+
+
+#media = int(input('digite sua media'))
+#taxa = int(input('sua taxa'))
+
+#if media >= 6 and taxa >= 75:
+#    print('vc foi aprovado')
+#else:
+#    print('vc n foi aprovado')
+
+
+
+
+
+#aprovado = media >= 6 and taxa >=75
+#print(aprovado)
+
+
+
+
+
+
