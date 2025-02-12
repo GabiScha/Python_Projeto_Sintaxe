9713ee24032521c2eae7bd5f59e7e2520fa9e4f1
Gabriela Schaper <gabrielasch2409@gmail.com>
2025-02-12 15:20:16 -0300

    End

--- Arquivos/app.py
+++ Arquivos/app.py
@@ -62,22 +62,94 @@
 
 # interromper while
 
-soma = 0
-cont = 0 
-while True:
-    nota = int(input('Digite uma nota ou -1 pra sair'))
-    if nota == -1:
-        break
+# soma = 0
+# cont = 0 
+# while True:
+#     nota = int(input('Digite uma nota ou -1 pra sair'))
+#     if nota == -1:
+#         break
 
-    soma = soma + nota
-    cont += 1
-    print(soma)
-
-
-print(f'a média da sala é: {soma/cont}')
+#     soma = soma + nota
+#     cont += 1
+#     print(soma)
 
 
+# print(f'a média da sala é: {soma/cont}')
 
+
+
+
+# FOR --
+
+# for i in range(0, 101):
+#     print(f'5 x {i} = {5 * i}')
+
+# for i in range(1, 1001, 1):
+#     if i % 2 == 0:
+#         print(i)
+
+
+#LISTAS --   
+
+# notas = []
+
+# while True:
+#     nota = int(input('Digite uma nota: '))
+#     if nota != -1:
+#         break
+#     notas.append(nota)
+
+
+# print(notas)
+
+# cont = 0
+# soma = 0
+# for i in notas:
+#     soma += 1 
+#     cont += 1
+
+# print(notas)
+# print(soma/cont)
+
+
+#DICIONARIO -- 
+
+# pessoa = {'nome': 'Marcos', 'idade': 38, 'altura': 1.78}
+
+# pessoa['cpf'] = '123131231'
+
+# print(pessoa.get('cpf', 'essa chave não existe'))
+
+# pessoas = {}
+# while True:
+#     nome = input()
+#     idade = input()
+
+#     if idade == -1:
+#         break
+#     dicionario = {'nome': nome, 'idade':  idade}
+#     pessoas.append(dicionario)
+
+# print(pessoas[0]['nome'])
+
+
+
+
+# FUNCÕES -- 
+
+# def calcula(n1, n2):
+#     print('Calculando...')
+#     print(f'a resposta é: {(n1 + n2)/2}')
+
+
+# print(calcula(2, 3))
+
+    
+
+#IMPORT --
+
+# import exemplo
+# boas_vindas = exemplo.exibir_boas_vindas();
 
 
 
---
+++ Arquivos/exemplo.py
@@ -0,0 +1,7 @@
+
+# Arquivo usado de exemplo no app.py que printa Bem Vindo!
+
+def exibir_boas_vindas():
+    print('Bem Vindo!')
+
+
